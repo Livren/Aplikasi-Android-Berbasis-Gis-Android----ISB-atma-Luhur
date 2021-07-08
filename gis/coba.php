b5bae8dc6abb651c2586e9209758c706f05df43b
Livren Sandi <76139758+Livren@users.noreply.github.com>
2021-07-08 08:49:28 +0700

    Add files via upload

---
+++ gis/coba.php
@@ -0,0 +1,21 @@
+<?php
+for ($a = 1; $a <=9; $a++) {
+            for($b = 1; $b <= $a; $b++){
+			echo "1";	
+			}
+                //menampilkan bintang dari kanan ke kiri
+            
+            echo"<br>";
+            //Memberikan baris baru atau enter pada
+			
+			for ($c = 1; $c <=9; $c++) {
+            for($d = 9; $d >= $c; $d--){
+			echo "1 &nbsp";	
+			}
+                //menampilkan bintang dari kanan ke kiri           
+            echo"<br>";
+            //Memberikan baris baru atau enter pada 
+        }			
+        }
+
+?>
